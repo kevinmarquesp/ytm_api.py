b10077af984e64edf8640b4f25b8045e423152e8
Kevin Marques <kevinmarqueskmp@gmail.com>
2024-05-26 10:57:55 -0300

    :sparkles: feature: created the search command function routine

--- ytm-api.py
+++ ytm-api.py
@@ -1,28 +1,85 @@
 #!/usr/bin/env python3
 
-from sys import argv
+from sys import argv, stderr
 from argparse import Namespace, ArgumentParser
+from json import dumps
 
 try:
     from ytmusicapi import YTMusic
 
 except ModuleNotFoundError as err:
-    print(f"\033[31m{err}\033[m")
+    stderr.write(f"[ERRO] {err}.\n")
 
     exit(1)
 
-VERSION = "0.1.0"
+
+# Helper functions and decorators.
+
+def command_error_logger(command: callable) -> callable:
+    """Just show the error message on the std error and then exit with status
+    code 1 if the function command decorated doesn't raise some exception.
+    """
+    def wrapper(*args):
+        try:
+            return command(*args)
+
+        except Exception as err:
+            stderr.write(f"[ERRO] {err}.\n")
+            exit(1)
+
+    return wrapper
 
 
 # The CLI related functions will be below.
 
+@command_error_logger
+def search(ytm: YTMusic, terms: list[str],
+           is_top_result_only: bool) -> dict | list[dict]:
+    """It will use the Youtube Music API to search for each term individually,
+    the final result will be stored in a list of dicts with "searchTerm" and
+    "searchResult" keys; Also, if the list has only one item (when the user
+    just search for one term) it will return the only dict of that list.
+    """
+    if len(terms) == 0:
+        raise Exception("No search therms specified")
+
+    results = []
+
+    for term in terms:
+        result = ytm.search(term)
+
+        if is_top_result_only:
+            result = list(filter(lambda elm: elm["category"] == "Top result",
+                                 result))
+
+        results.append({
+            "searchTerm": term,
+            "searchResult": result
+        })
+
+    if len(results) == 0:
+        raise Exception("Could not find anything")
+    elif len(results) == 1:
+        return results[0]
+
+    return results
+
+
 def main(args: Namespace) -> None:
     """Giving the command line arguments, already parsed, this funcion will
     figure out which subcommand routine it should run and display the result on
-    the standart output. It will be respeonsible to handle the errors if
-    needed.
+    the standart output.
     """
-    print(args)
+    JSON_INDENT_SIZE = 2
+
+    ytm = YTMusic()
+
+    match args.subcommand:
+        case "search":
+            result = search(ytm, args.terms, args.top_result_only)
+            result_json = dumps(result, indent=JSON_INDENT_SIZE)
+
+            print(result_json)
 
 
 def parse_app_args(args: list[str]) -> Namespace:
@@ -31,6 +88,8 @@ def parse_app_args(args: list[str]) -> Namespace:
     keep each subcommand parser separated by a commentary to improve
     readability.
     """
+    VERSION = "0.1.0"
+
     # Parser and subparser definition, global flags should be here.
     parser = ArgumentParser(prog="ytm-api", description="Python script created\
                             to easely fetch data from the Youtube Music API\
